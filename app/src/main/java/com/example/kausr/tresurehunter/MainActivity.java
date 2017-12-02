bd7db353f4eba00c2b0afa3168a4e84c47f36a11
Kaus <kausr2595@gmail.com>
2017-12-02 12:33:23 +0530

    Firebase Update

--- app/src/main/java/com/example/kausr/tresurehunter/MainActivity.java
+++ app/src/main/java/com/example/kausr/tresurehunter/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.kausr.tresurehunter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
